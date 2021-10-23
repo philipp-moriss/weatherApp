2c0458b229d6306d008f9c35fca198f5961c8148
philipp-moriss <filverem@gmail.com>
2021-10-23 09:38:12 +0300

    update add create thunk and redux

--- src/API.ts
+++ src/API.ts
@@ -6,16 +6,43 @@ const configWether = {
 const configIMG = {
     baseURL: "https://openweathermap.org/img/wn/",
 };
-const key:string = "31957e009d412dacba96adecca04b57c";
+const key: string = "31957e009d412dacba96adecca04b57c";
 const axiosInstance = axios.create(configWether);
 const axiosInstanceforIMG = axios.create(configIMG);
 
+type GetWeatherType = {
+    base: string;
+    clouds: { all: number };
+    cod: number;
+    coord: { lon: number, lat: number };
+    dt: number;
+    id: number;
+    main: { temp: number, feels_like: number, temp_min: number, temp_max: number, pressure: number, sea_level: number };
+    name: string;
+    rain: any;
+    sys: {
+        type: number;
+        id: number,
+        country: string,
+        sunrise: number,
+        sunset: number
+    };
+    timezone: number;
+    weather: Array<weatherType>
+    visibility: number;
+    wind: { speed: number, deg: number, gust: number }
+}
+
+type weatherType = {
+    id: number, main: string, description: string, icon: string
+}
+
 const API = {
     searchWeatherByCiti: (citi: string) => {
-        return axiosInstance.get(`weather?q=${citi}&units=metric&appid=${key}`)
+        return axiosInstance.get<GetWeatherType>(`weather?q=${citi}&units=metric&appid=${key}`)
     },
-    searchImgBYIcon:(icon:string)=>{
-        return axiosInstanceforIMG.get(`${icon}@2x.png`)
+    searchImgBYIcon: (icon: string) => {
+        return axiosInstanceforIMG.get<weatherType>(`${icon}@2x.png`)
     }
 };
 
